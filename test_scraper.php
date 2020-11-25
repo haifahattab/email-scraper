49c83975a54f9b2c266f43dee6deac7368a35db0
haifahattab <hattab_haifa@yahoo.com>
2020-11-25 15:57:15 +0100

    test

--- test_scraper.php
+++ test_scraper.php
@@ -2,5 +2,5 @@
 include 'email_scraper.php';
 $url='https://github.com/nyxgeek/username-lists/blob/master/usernames-top100/usernames_gmail.com.txt';
 $emails = scrape_email($url);
-echo implode($emails,'<br>');
+echo implode($emails,'');
 ?>
